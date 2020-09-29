7d919e5aaaca926f4e3b762fd151dcda65707eae
RazzleJazzled <robertjgassner@gmail.com>
2020-09-28 17:21:24 -0700

    fixed server error

--- server.js
+++ server.js
@@ -25,10 +25,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
